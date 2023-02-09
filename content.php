a8ad4393d05b4b0f2d514d4af45aea1f383c9ac0
Shahram Rahatlou <rahatlou@gmail.com>
2023-02-09 12:54:45 +0100

    first version of this webpage

---
+++ content.php
@@ -0,0 +1,87 @@
+<div id="content">
+	<h1>Particle Physics</h1>
+  <h3>A.A. 2022-23</h3>
+
+	<div class="post news-cont">
+	   <? /*php include("Didattica/FNSN/news.html") */?>
+	</div>
+
+	<div class="post">
+		<p><b>Lectures start on Monday 27 Feb 2023</b>
+<?php
+/*
+			<p>Le lezioni  si svolgeranno in modalita` mista come previsto dall'ateneo.
+				 Tutte le lezioni saranno trasmesse in diretta sulla piattaforma Google meet.</p>
+				 <p>Per seguire le lezioni in presenza e` richiesta la prenotazione su
+					 <a href="https://prodigit.uniroma1.it/">prodigit</a>.
+					 Sara` effettutato l'appello all'inizio di ogni lezione.</p>
+					 */ ?>
+
+			<p>All communicaztion and news will appear on Google Classroom.
+				All students attending the course are kindly asked to register on
+					<a href="https://classroom.google.com/c/NTU5MjU5NDIxMjQ5?cjc=3l3e5j7"  target="_blank">google classroom</a> using the class code
+					<a href="https://classroom.google.com/c/NTU5MjU5NDIxMjQ5?cjc=3l3e5j7"  target="_blank">3l3e5j7</a>.
+			  You can register  <b>exclusively</b> with your uniroma1 credentials (no personal google account).
+			</p>
+
+<?php /*
+<p>Si puo` richiedere l'accesso alla registrazione di singole lezioni tramite questa
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSf3SxqXdfrWfspBT7wWdodC2sYLiyN4jlWjnwcFNZJ51V7Xew/viewform?usp=sf_link">google form</a>.</p>
+*/?>
+				<!--p>Il ricevimento si volge tramite Meet nell'orario indicato oppure in presenza per appuntamento.</p-->
+
+
+
+
+
+
+	<h2 class="title">Lecture schedule</h2>
+	<div class="entry">
+			<ul>
+	  	<li> <b>Monday, 12-14, Aula Conversi </b>, Edificio Marconi (CU013) </li>
+	  	<li> <b>Wednesday  10-12, Aula Conversi </b>, Edificio Marconi (CU013) </li>
+			<li> Friday 9-10, Aula Majorana, Edificio Marconi (CU013) (Not on weekly basis and to be confirmed when necessary) </li>
+		</ul>
+	</div>
+
+	<h2 class="title">Lectures</h2>
+	<div class="entry">
+
+		<table style="width:90%">
+		  <tr>
+		    <th style="width:15%"> Date</th>
+		    <th style="width:70%">Topic</th>
+		    <th style="width:15%">Material</th>
+		  </tr>
+
+
+			<!--
+			<tr>
+				<td>2022- </td>
+				<td> </td>
+				<td><a href="https://github.com/rahatlou/FNSN/blob/FNSN2022/materiale/2022-NN-MM-lezione.pdf" target=blank_>pdf</a></td>
+			</tr>
+
+
+			-->
+
+
+		  <tr>
+		    <td>2023-02-27 </td>
+		    <td>Introduction. syllabus. course plan. Collisions and decays.</td>
+		    <td><a href="" target=blank_></a></td>
+      </tr>
+
+
+		</table>
+
+
+
+
+
+	</div> <!--- end of entry -->
+
+
+
+</div> <!--- end of post -->
+</div> <!-- end content -->
---
+++ sidebar.php
@@ -0,0 +1,41 @@
+<div id="sidebar-content">
+
+<h3 class=h3sidebar>Last update: 9 Feb 2023</h3>
+  Created by
+  <a href="http://www.roma1.infn.it/people/rahatlou/">Shahram Rahatlou</a>
+  [<a href="mailto:shahram.rahatlou@uniroma1.it">shahram.rahatlou (at) uniroma1.it</a>]
+
+<p>
+<a href="https://classroom.google.com/c/NTU5MjU5NDIxMjQ5?cjc=3l3e5j7"  target="_blank">Google classroom: 3l3e5j7</a><br>
+<a href="https://calendar.google.com/calendar/u/0/r?cid=c_classroomd30d228b@group.calendar.google.com">Calendario Google del corso</a></br>
+
+
+<p>Syllabus and exam sessions:
+<a href="https://corsidilaurea.uniroma1.it/it/view-course-details/2022/30055/20190322084705/bb8df54f-be46-4230-bd6e-aec49b6b9cf1/ed6e9474-40d7-4d78-98ee-2457068eff6e/0823b198-fe5e-4797-a86d-56da889c58c2/9daa0d75-32bf-4bc4-8139-d0a5d38211f2?guid_cv=ed6e9474-40d7-4d78-98ee-2457068eff6e&current_erogata=bb8df54f-be46-4230-bd6e-aec49b6b9cf1" target="_blank">catalogo Sapienza</a>
+</p>
+
+<h2>Office Hours</h2>
+  <ul>
+    <li>
+      Wednesday 13:30-15:30, <a href="http://www.phys.uniroma1.it/mappe/mappe.php?edificio=marconi&piano=2">Stanza 241, Piano 2, Edificio Marconi (CU013)</a> oppure per appuntamento</li>
+  </ul>
+
+<div class="post news">
+  <?php/* include("Didattica/ParticlePhysics/news.html") */?>
+</div>
+
+  <h2>Bibliography and reference books</h2>
+  <div style="height: 400px; overflow: auto">
+  <ul>
+    <li>Francesco Terranova, <a href="https://global.oup.com/academic/product/a-modern-primer-in-particle-and-nuclear-physics-9780192845245?cc=it&lang=en&">A modern primer in particle and nuclear physics</a></li>
+    <li>R. Paramatti, <a href="http://cern.ch/ric/cinematica.pdf">Dispense di cinematica relativistica</a></li>
+    <li>D. Griffiths, <a href="https://www.wiley.com/en-us/Introduction+to+Elementary+Particles+%2C+2nd%2C+Revised+Edition-p-9783527406012">Introduction to Elementary Particles , 2nd Ed.</a></li>
+    <li>A. Das and T. Ferbel, <a href="https://www.worldscientific.com/worldscibooks/10.1142/5460">Introduction to Nuclear and Particle Physics, 2nd ed.</a></li>
+    <li>D. H. Perkins, <a href="https://doi.org/10.1017/CBO9780511809040">Introduction to High Energy Physics, 4th ed.</a></li>
+    <li>Cahn and Goldhaber, <a href="https://doi.org/10.1017/CBO9780511609923">The experimental foundation of Particle Physics</a>, 2nd Ed.</li>
+    <li>J. J. Sakurai, J. Napolitano, <a href="https://www.zanichelli.it/ricerca/prodotti/meccanica-quantistica-moderna-sakurai-napolitano">Meccanica quantistica moderna, 2nd Ed.</a></li>
+  </ul>
+  </div>
+
+
+</div> <!-- sidebar -->
