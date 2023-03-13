796be2bbed1f09c64147e0710cbf1b9d5f59c9a2
Shahram Rahatlou <rahatlou@ShaBookPro.fritz.box>
2023-03-13 17:19:43 +0100

    link to 13/3

--- content.php
+++ content.php
@@ -95,10 +95,18 @@
 
 			<tr>
 		    <td style="text-align: center">2023-03-13 </td>
-		    <td>Static quark model. Meson and baryon multiplets. Producion and decay. Light meson mixing.
+		    <td>Static quark model. Meson multiplets. Producion and decay. Light meson mixing.
+				(Terranova Ch.8, Perkins Ch. 4, Griffiths Ch. 4,
+				<a href="https://pdg.lbl.gov/2022/reviews/rpp2022-rev-quark-model.pdf">PDG ch. 15</a>)</td>
+		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/2023-03-13–lecture-quark-model.pdf" target=blank_>pdf</a></td>
+			</tr>
+			<tr>
+		    <td style="text-align: center">2023-03-15 </td>
+		    <td>Static quark model. Baryon multiplets. Prediction and discovery of &Sigma;<sup>-</sup>.
 				(Terranova Ch.8, Perkins Ch. 4, Griffiths Ch. 4,
 				<a href="https://pdg.lbl.gov/2022/reviews/rpp2022-rev-quark-model.pdf">PDG ch. 15</a>)</td>
 		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/" target=blank_></a></td>
+			</tr>
 
 		</table>
 
