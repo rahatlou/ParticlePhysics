26b4700378744f1b6680390455daddff7d77cbc9
Shahram Rahatlou <rahatlou@gmail.com>
2023-05-03 16:22:48 +0200

    update after 3/5

--- content.php
+++ content.php
@@ -226,10 +226,10 @@
 
 			<tr>
 		    <td style="text-align: center">2023-05-03</td>
-		    <td>Discovery of W<sup>&pm;</sup> and Z<sup>0</sup>.
+		    <td>Discovery of W<sup>&pm;</sup> and Z<sup>0</sup> at CERN
 				(Griffiths Ch. 9, Terranova Ch. 12, Perkins Ch. 9, Goldhaber Ch. 6)
 			 </td>
-		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/" target=blank_></a></td>
+		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/2023-05-03-lecture-weak.pdf" target=blank_>board</a></td>
 			</tr>
 
 
