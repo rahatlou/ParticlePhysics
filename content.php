897c251534ca6850da9cbdcb7c0117f937ccdd74
Shahram Rahatlou <rahatlou@gmail.com>
2023-05-17 17:39:48 +0200

    update after 17/5

--- content.php
+++ content.php
@@ -265,9 +265,9 @@
 
 			<tr>
 		    <td style="text-align: center">2023-05-17</td>
-		    <td>Inelastic electron-nucleon scattering. Form factors and structure functions.
+		    <td>Inelastic electron-nucleon scattering. Form factors. Goldhaber Ch. 8, Perkins Ch. 5)
 			 </td>
-		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/" target=blank_></a></td>
+		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/2023-05-17-lecture-scattering.pdf" target=blank_>board</a></td>
 			</tr>
 
 			<tr>
