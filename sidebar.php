240a3e85e264b20daae980dfb8d38355e20a40a1
Shahram Rahatlou <rahatlou@gmail.com>
2024-02-21 10:48:32 +0100

    link to 2023 website

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,7 @@
 <p>Syllabus and exam sessions:
 <a href="https://corsidilaurea.uniroma1.it/it/view-course-details/2023/32384/20190322090929/b11487b2-73a1-4c99-a3cc-2028e7f41e1a/9f2cf951-1b4b-4f0e-8789-9962791dee1b/0823b198-fe5e-4797-a86d-56da889c58c2/9daa0d75-32bf-4bc4-8139-d0a5d38211f2?guid_cv=9f2cf951-1b4b-4f0e-8789-9962791dee1b&current_erogata=b11487b2-73a1-4c99-a3cc-2028e7f41e1a" target="_blank">catalogo Sapienza</a>
 </p>
+<p>Previos years: <a href="index.php?link=Didattica&sublink=2023.ParticlePhysics">2023</a></p>
 
 <h2>Office Hours</h2>
   <ul>
