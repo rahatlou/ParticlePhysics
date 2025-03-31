7495f59017f5a1a4e466489edcae70a2c8e484e7
Shahram Rahatlou <rahatlou@gmail.com>
2025-03-31 21:43:28 +0200

    update on 31/3. static quark model

--- content.php
+++ content.php
@@ -195,9 +195,19 @@
 		  <td style="text-align: center">2025-03-31  </td>
 			<td>
 				Static quark model. Meson multiplets. Light meson mixing. Decay of &rho;<sup>0</sup>. Baryon multiplets.
-		  	(Terranova Ch. 5 and 8, Griffiths Ch. 4,  Perkins Ch. 4)
+		  	(Terranova Ch. 8, Griffiths Ch. 4,  Perkins Ch. 4)
 			</td>
-			<td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/PP2025/material/2025-03-31-lecture-12.pdf" target=blank_></a></td>
+			<td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/PP2025/material/2025-03-31-lecture-12.pdf" target=blank_>board</a></td>
+		</tr>
+
+		<tr>
+		  <td style="text-align: center">2025-04-01</td>
+			<td>
+				Static quark model. Baryon multiplets. Prediction and discovery of &Omega;<sup>-</sup>. Baryon decays.
+					Hypothesis of new quantum number: color. Baryon decuplet.
+					(Terranova Ch. 8, Griffiths Ch. 4,  Perkins Ch. 4)
+			</td>
+			<td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/PP2025/material/2025-04-01-lecture-13.pdf" target=blank_></a></td>
 		</tr>
 
 
@@ -214,19 +224,10 @@
       </tr>
 
 
-			<tr>
-		    <td style="text-align: center">2024-03-25 </td>
-		    <td>Static quark model. Meson multiplets. Producion and decay. Light meson mixing. Decay of &rho;<sup>0</sup>
-				(Terranova Ch.8, Goldhaber Ch. 5, Perkins Ch. 4, Griffiths Ch. 4,
-				<a href="https://pdg.lbl.gov/2022/reviews/rpp2022-rev-quark-model.pdf"  target=blank_>PDG ch. 15</a>, <a href="https://pdglive.lbl.gov"  target=blank_>PDGlive</a>,
-				<a href="https://pdg.lbl.gov/2002/clebrpp.pdf"  target=blank_>Clebsch-Gordan coefficients</a>)</td>
-		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/2024-03-25-lecture-11.pdf" target=blank_>board</a></td>
-			</tr>
 
 			<tr>
 		    <td style="text-align: center">2024-03-27 </td>
-		    <td>Static quark model. Baryon multiplets. Prediction and discovery of &Omega;<sup>-</sup>. Baryon decays.
-					Hypothesis of new quantum number: color. Baryon decuplet.
+		    <td>
 				(Terranova Ch.8, Goldhaber Ch. 5, Perkins Ch. 4, Griffiths Ch. 4,
 				<a href="https://pdg.lbl.gov/2022/reviews/rpp2022-rev-quark-model.pdf">PDG ch. 15</a>)</td>
 		    <td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/main/material/2024-03-27-lecture-12.pdf" target=blank_>board</a></td>
