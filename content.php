6488b04e8ec16b09c671417171f9710832960c17
Shahram Rahatlou <rahatlou@gmail.com>
2025-04-28 14:21:52 +0200

    reference for weak intercactions

--- content.php
+++ content.php
@@ -275,8 +275,8 @@
 			<td style="text-align: center">2025-04-28</td>
 			<td>
 				Decay of q-qbar resonances: Decays of J/&Psi;. Disovery of the &tau; lepton.
-				Discovery of the bottom quark and the &Upsilon; (b-bbar) resonances. 
-				(Cahn-Goldhaber Ch. 9, 11, 14)
+				Discovery of the bottom quark and the &Upsilon; (b-bbar) resonances.
+				(Cahn-Goldhaber Ch. 9, 11)
 			</td>
 			<td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/PP2025/material/2025-04-28-lecture-19.pdf" target=blank_>board</a></td>
 		</tr>
@@ -285,8 +285,8 @@
 		<tr>
 			<td style="text-align: center">2025-04-29</td>
 			<td>
-				Discovery of the top quark. Weak interactions.
-				(Cahn-Goldhaber Ch. 9, 11, 14)
+				Discovery of the top quark. (Cahn-Goldhaber Ch. 14). Weak interactions. Number of neutrino flavors.
+				(Terranova Ch. 10, Cahn-Goldhaber Ch. 14, 6, Griffiths Ch. 9, Perkins Ch. 9)
 			</td>
 			<td style="text-align: center"><a href="https://github.com/rahatlou/ParticlePhysics/blob/PP2025/material/2025-04-28-lecture-20.pdf" target=blank_></a></td>
 		</tr>
